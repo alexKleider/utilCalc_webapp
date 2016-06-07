00fcde73318903f47a94bda7857c0c7d08e4c832
Alex Kleider <alex@kleider.ca>
2016-06-07 09:10:34 -0700

    Save before attempt to split the js code.
    Want to try splitting the js code into two files:
    One containing all the html dependent (jQuery) code
    and the other with code that can be run by node.js.
    This commit will serve as a return point if the project fails.

--- scripts/jsCost.js
+++ scripts/jsCost.js
@@ -2,50 +2,53 @@
 //    file ~/Encrypted/GHRental/Meters/jsCost.js
 "use strict";
 const galInCuFt = 0.0278;
+const kwhPrice1 = 0.18212;
+const kwhPrice2 = 0.25444;
+const kwhPrice3 = 0.37442;
+const summerBase = 7.0;  //| 'Basic' (E6/E1)
+const winterBase = 8.5;  //| usage in kWh/day.
 
-/*  Data in CSV format:
-Date,cu_ft,kwh,current price of propane/gal,paid,comment
-2016-03-19,30.1,59466,N/A,N/A,Initial reading
-2016-04-16,853.7,59708,$4.079,$181.35,1st reading 
-2016-05-23,1691.8,60063,$3.379,$105.21,2nd reading
+const winterMonths = [11, 12, 1, 2, 3, 4];
 
-In JSON notation: */
-var dataStr = [
-{"date": "2016-03-19","gas": 30.1, "cost": 0,
-                        "kwh": 59466, "paid": 0},
-{"date": "2016-04-16","gas": 853.7, "cost": 4.079,
-                        "kwh": 59708, "paid": 181.35},
-{"date": "2016-05-23","gas": 1691.8, "cost": 3.379,
-                        "kwh": 60063, "paid": 105.21},
-]
+function daysInFebruary(year){
+    var yr = Number(year);
+    if (yr == NaN){
+        console.log("Bad input for a year.");
+        return;
+    }
+    if (yr < 2000){
+        console.log("Probably an invalid (<2000) year.");
+    }
+    if (yr % 400 ==0) return 29;
+    if (yr % 100 ==0) return 28;
+    if (yr % 4 ==0) return 29;
+    return 28;
+}
 
-//var dataJson = JSON.parse(dataStr)
-
-var gas = {
-    prevReading: 853.7,
-    curReading: 1691.8,
-    cost: 3.379,
-    };
-
-function getElectricity(){
-    return {
+var protoInterval = {
+    dates: {
+        prev: {
+        yr: 2016,
+        mo: 0,
+        d: 0,
+        },
+        cur: {
+        yr: 2016,
+        mo: 0,
+        d: 0,
+        },
+    },
+    gas: {
+        prev: 0,
+        cur: 0,
+        cost: 0
+    },
+    kwh: {
+        prev: 0,
+        cur: 0,
     }
 }
 
-var electricityDefaults = {
-    prevReading: 59708,
-    curReading: 60063,
-    date1: {
-        yr: 2016,
-        mo: 4,
-        d: 16,
-        },
-    date2: {
-        yr: 2016,
-        mo: 5,
-        d: 23,
-        },
-}
 
 function calculate(){
     var gasResult = document.getElementById('gasResult');
@@ -53,85 +56,22 @@ function calculate(){
     disablePreviousReading(false);
 }
 
-function doNOTgetJson(){
-    document.write("Not executing 'getJson()'.");
-}
-
-function getJson(){
-  var jsonData, rawJsonElement;
-  var resultPage = document.getElementById('results');
-  while (true){
-    rawJsonElement = document.getElementById('readings');
-    try{
-    jsonData = JSON.parse(rawJsonElement.textContent);
-    resultPage.textContent = rawJsonElement.textContent;
-    break
-    }catch(e){
-        if (e instanceof SyntaxError){
-            var newText = 
-            "WHAT YOU ENTERED ISN'T VALID JSON!\n Try again.\n"
-            + rawJsonElement.textContent;
-            rawJsonElement.textContent = newText;
-        }
-    }
-  }
-  var output = [];
-  jasonData.forEach(function(item){
-        output.push(item.date);
-        output.push(item.gas);
-        output.push(item.cost);
-        output.push(item.kwh);
-        output.push(item.paid);
-        output.push('');
-  });
-  resultPage.textContent = output;
-  disableCalculate(false);
-}
-
-function check4Storage(){
-    if(typeof(Storage)!=="undefined"){
-        console.log("It's OK.");
-console.log("End of script.");
-    }
-    else{
-        console.log("Too bad.");
-    }
-}
-
-
 var conversionFactor = document.getElementById("conversionFactor");
 conversionFactor.textContent = String(galInCuFt) +
                                " Gallons per Cubic Ft";
-var prevDate = document.getElementById("prevDate");
-var curDate = document.getElementById("curDate");
-var kwhPrev = document.getElementById("kwhPrev");
-var kwhCur = document.getElementById("kwhCur");
-var gasCost = document.getElementById("gasCost");
-var gasPrev = document.getElementById("gasPrev");
-var gasCur = document.getElementById("gasCur");
 
-check4Storage();
-
-// Rest of file comes from Fa1:
-
-
-"use strict";
 var fileContent = '';
-var resultParagraph = document.getElementById("download");
-console.log("resultParagraph currently contains:");
-console.log(resultParagraph.textContent);
+var report = document.getElementById("report");
 
 function onChange(event){
+    // Assigns fileContent in response to
+    // a change in input file assignation.
     console.log("You've changed files.");
     var file = event.target.files[0];
     var reader = new FileReader();
     reader.onload = function(event){
-    fileContent = event.target.result;
-    resultParagraph.textContent = fileContent;
-    console.log("Returned result is:");
-    console.log(fileContent);
-    console.log("resultParagraph currently contains:");
-    console.log(resultParagraph.textContent);
+        fileContent = event.target.result;
+        report.textContent = fileContent;
     }
     reader.readAsText(file);
     disableCalculate(false);
@@ -140,11 +80,21 @@ function onChange(event){
 }
 
 function processJsonStr(){
+    // Attempts to convert fileContent into
+    // a json object consisting of an array
+    // of readings; if successful, each pair 
+    // of these readings is made into a 
+    // protoInterval type object and an array
+    // (length one less than the json)
+    // is returned.
+    // If unsuccessful, returns no value.
+    // Populates the report ("Result of
+    // File Choice:") window.
     var jsonObj;
     try{jsonObj = $.parseJSON(fileContent);}
     catch(e){
         if (e instanceof SyntaxError){
-            resultParagraph.textContent = 
+            report.textContent = 
             "Parsing JSON file failed due to a syntax error"
             + /*" on line # " +  e.linenumber + */
             ". Fix/edit/change the file and try again.";
@@ -155,8 +105,9 @@ function processJsonStr(){
         } else {throw(e); }
     }
     var nItems = jsonObj.length;
-    resultParagraph.textContent = "There are "+nItems+" items.";
+    report.textContent = "There are "+nItems+" items.";
     var output = ["There are "+nItems+" items.\n"];
+
     jsonObj.forEach(function(item){
         output.push(item.date);
         output.push(item.gas);
@@ -165,7 +116,8 @@ function processJsonStr(){
         output.push(item.paid);
         output.push('');
     });
-    resultParagraph.textContent = output;
+
+    report.textContent = output;
     disableMoveBack(false);
     disableMoveForward(true);
 }
